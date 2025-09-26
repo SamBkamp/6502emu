a15c52c7e8cad4336b0c3bbb25617ff3f02da349
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-26 22:59:00 +0800

    added cpu state to function calls

--- main6502.c
+++ main6502.c
@@ -9,42 +9,41 @@
 #include "pins.h"
 #include "opcodes.h"
 
-void print_registers(){
-  printf("X: %x\nY: %x\n", registers[X], registers[Y]);
+void print_registers(context *c){
+  printf("X: %x\nY: %x\n", c->registers[X], c->registers[Y]);
 }
 
 void fetch_data(void){
   data_bus = RAM[address_bus];
 }
 
-
-void reset(void){
+void reset(context *c){
   address_bus = 0xFFFC;
   fetch_data();
-  registers[PC+1] = data_bus;
+  c->registers[PC+1] = data_bus;
   
   address_bus = 0xFFFD;
   fetch_data();
-  registers[PC] = data_bus;
+  c->registers[PC] = data_bus;
 }
 
-void step(void){ //step through instructions
-  print_registers();
-  address_bus = ((uint16_t)registers[PC] << 8) + registers[PC+1];
+void step(context *c){ //step through instructions
+  address_bus = ((uint16_t)c->registers[PC] << 8) + c->registers[PC+1];
   fetch_data();  
   printf("0x%04X : 0x%02X %s\n", address_bus, data_bus, opcodes[data_bus].name);
 
 
   if(opcodes[data_bus].func != NULL){
-    (*opcodes[data_bus].func)();
+    (*opcodes[data_bus].addr_mode)(c); //set addressing mode
+    (*opcodes[data_bus].func)(c); //call function associated with opcode
   }
 
-  registers[PC+1] += 1;
+  c->registers[PC+1] += 1;
 }
 
-void load_into_pc(uint16_t v){
-  registers[PC] = (v >> 8) & 0xff;
-  registers[PC+1] = v & 0xff;
+void load_into_pc(context *c, uint16_t v){
+  c->registers[PC] = (v >> 8) & 0xff;
+  c->registers[PC+1] = v & 0xff;
 }
 
 void load_uint16_into_ram(uint16_t addr, uint16_t val){
@@ -53,6 +52,11 @@ void load_uint16_into_ram(uint16_t addr, uint16_t val){
 }
 
 int main(int argc, char* argv[]){
+
+  context c = {
+    .registers = {0},
+    .ea = 0
+  };
   
   RAM = (uint8_t *)mmap(NULL, 65536, PROT_READ|PROT_WRITE|PROT_EXEC,
 			MAP_PRIVATE|MAP_ANON ,-1, 0);
@@ -64,11 +68,11 @@ int main(int argc, char* argv[]){
   RAM[0x8000] = 0xa2;
   RAM[0x8001] = 0x41;
   
-  reset();
-  step();  
-  step();
-  step();
-  step();
-
+  reset(&c);
+  step(&c);  
+  step(&c);
+  step(&c);
+  step(&c);
+  print_registers(&c);
   
 }
--- prot.h
+++ prot.h
@@ -3,14 +3,14 @@
 #define PROT_6520
 //prototypes for global functions and variables
 
-uint8_t registers[10]; //all cpu registers
+typedef struct{
+  uint8_t registers[10]; //all registers
+  uint16_t ea; //effective address
+}context;
+
 uint8_t *RAM;
 
-void reset(void);
-void fetch_data(void);
-void print_registers();
-void step(void);
-void load_into_pc(uint16_t v);
+void load_into_pc(context *c, uint16_t v);
 void load_uint16_into_ram(uint16_t addr, uint16_t val);
 
 #endif
