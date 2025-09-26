411d7599b5d5ad0b4edf98fc2d19dda8cc6f2384
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-26 18:05:08 +0800

    changed to fixed-width numbers and set data bus

--- main6502.c
+++ main6502.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "registers.h"
+#include <sys/mman.h>
+#include <stdint.h>
 
-char registers[REG_SIZE]; //all cpu registers
+#include "registers.h"
+#include "pins.h"
+
+uint8_t registers[REG_SIZE]; //all cpu registers
+uint8_t *RAM;
+
+void reset(void){
+  registers[PC] = RAM[0xFFFC];
+  registers[PC+1] = RAM[0xFFFD];
+}
+
+void step(void){ //step through instructions
+  printf("pc: 0x%02x%02x\n", registers[PC], registers[PC+1]);
+}
 
 int main(int argc, char* argv[]){
+  
+  RAM = (uint8_t *)mmap(NULL, 65536, PROT_READ|PROT_WRITE|PROT_EXEC,
+		     MAP_PRIVATE|MAP_ANON ,-1, 0);
+
+  data_bus[0] = 0xea;
+  
+  RAM[0xFFFC] = data_bus[0];
+  RAM[0xFFFD] = data_bus[0];
+
+  reset();
+  step();
+
   
 }
--- pins.h
+++ pins.h
@@ -1,21 +1,21 @@
 #ifndef PINS_6502
 #define PINS_6502
 
-char address_bus[2];
-char data_bus[1];
+uint8_t address_bus[2];
+uint8_t data_bus[1];
 
 //TODO: combine these into 1 byte with masks
-char BE; //bus enable
-char IRQB; //interrupt request
-char MLB; //memory lock
-char NMIB; //non-maskable interrupt
-char RWB; //read/write
-char RDY; //ready
-char RESB; //reset
-char SOB; //set overflow
+uint8_t BE; //bus enable
+uint8_t IRQB; //interrupt request
+uint8_t MLB; //memory lock
+uint8_t NMIB; //non-maskable interrupt
+uint8_t RWB; //read/write
+uint8_t RDY; //ready
+uint8_t RESB; //reset
+uint8_t SOB; //set overflow
 //TODO until here
 
-char SYNC; //SYNChronize with OpCode fetch
+uint8_t SYNC; //SYNChronize with OpCode fetch
 
 //ignoring ground/VSS because lol
 
