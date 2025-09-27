9534a4370965e55ad809b9ed63d90dfc7263e858
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-28 00:29:14 +0800

    CHORE: updating random main things

--- main6502.c
+++ main6502.c
@@ -19,8 +19,8 @@ opcode opcodes[] = {
   {OP_bvs, "bvs", addr_pcr}, {NULL, "adc", NULL}, {OP_nop, "nop25", addr_implied}, {NULL, "rra", NULL}, {OP_nop, "nop26", addr_implied}, {NULL, "adc", NULL}, {NULL, "ror", NULL}, {NULL, "rra", NULL}, {OP_sei, "sei", addr_implied}, {NULL, "adc", NULL}, {OP_ply, "ply", addr_stack}, {NULL, "rra", NULL}, {OP_nop, "nop28", addr_implied}, {NULL, "adc", NULL}, {NULL, "ror", NULL}, {NULL, "rra", NULL}, 
   {OP_bra, "bra", addr_pcr}, {NULL, "sta1", NULL}, {OP_nop, "nop29", addr_implied}, {NULL, "sax", NULL}, {NULL, "sty", NULL}, {OP_sta, "sta2", addr_zp}, {NULL, "stx", NULL}, {NULL, "sax", NULL}, {NULL, "dey", NULL}, {OP_nop, "nop30", addr_implied}, {NULL, "txa", NULL}, {OP_nop, "nop31", addr_implied}, {NULL, "sty", NULL}, {OP_sta, "sta8", addr_abs}, {NULL, "stx", NULL}, {NULL, "sax", NULL}, 
   {OP_bcc, "bcc", addr_pcr}, {NULL, "sta3", NULL}, {OP_sta, "sta", addr_zp_indirect}, {OP_nop, "nop33", addr_implied}, {NULL, "sty", NULL}, {NULL, "sta4", NULL}, {NULL, "stx", NULL}, {NULL, "sax", NULL}, {NULL, "tya", NULL}, {NULL, "sta6", NULL}, {NULL, "txs", NULL}, {OP_nop, "nop34", addr_implied}, {OP_nop, "nop35", addr_implied}, {NULL, "sta5", NULL}, {OP_nop, "nop36", addr_implied}, {OP_nop, "nop37", addr_implied}, 
-  {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {OP_ldx, "ldx", addr_imm}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx", NULL}, {NULL, "lax", NULL}, {NULL, "tay", NULL}, {NULL, "lda", NULL}, {NULL, "tax", NULL}, {OP_nop, "nop38", addr_implied}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx", NULL}, {NULL, "lax", NULL}, 
-  {OP_bcs, "bcs", addr_pcr}, {NULL, "lda", NULL}, {OP_nop, "nop39", addr_implied}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx", NULL}, {NULL, "lax", NULL}, {NULL, "clv", NULL}, {NULL, "lda", NULL}, {NULL, "tsx", NULL}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx", NULL}, {NULL, "lax", NULL}, 
+  {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {OP_ldx, "ldx", addr_imm}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {OP_ldx, "ldx4", addr_zp}, {NULL, "lax", NULL}, {NULL, "tay", NULL}, {NULL, "lda", NULL}, {NULL, "tax", NULL}, {OP_nop, "nop38", addr_implied}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx3", NULL}, {NULL, "lax", NULL}, 
+  {OP_bcs, "bcs", addr_pcr}, {NULL, "lda", NULL}, {OP_nop, "nop39", addr_implied}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx1", NULL}, {NULL, "lax", NULL}, {NULL, "clv", NULL}, {NULL, "lda", NULL}, {NULL, "tsx", NULL}, {NULL, "lax", NULL}, {NULL, "ldy", NULL}, {NULL, "lda", NULL}, {NULL, "ldx2", NULL}, {NULL, "lax", NULL}, 
   {NULL, "cpy", NULL}, {NULL, "cmp", NULL}, {OP_nop, "nop40", addr_implied}, {NULL, "dcp", NULL}, {NULL, "cpy", NULL}, {NULL, "cmp", NULL}, {NULL, "dec", NULL}, {NULL, "dcp", NULL}, {NULL, "iny", NULL}, {NULL, "cmp", NULL}, {NULL, "dex", NULL}, {OP_nop, "nop41", addr_implied}, {NULL, "cpy", NULL}, {NULL, "cmp", NULL}, {NULL, "dec", NULL}, {NULL, "dcp", NULL}, 
   {OP_bne, "bne", addr_pcr}, {NULL, "cmp", NULL}, {OP_nop, "nop42", addr_implied}, {NULL, "dcp", NULL}, {OP_nop, "nop43", addr_implied}, {NULL, "cmp", NULL}, {NULL, "dec", NULL}, {NULL, "dcp", NULL}, {NULL, "cld", NULL}, {NULL, "cmp", NULL}, {OP_phx, "phx", addr_stack}, {NULL, "dcp", NULL}, {OP_nop, "nop45", addr_implied}, {NULL, "cmp", NULL}, {NULL, "dec", NULL}, {NULL, "dcp", NULL}, 
   {NULL, "cpx", NULL}, {NULL, "sbc", NULL}, {OP_nop, "nop46", addr_implied}, {NULL, "isb", NULL}, {NULL, "cpx", NULL}, {NULL, "sbc", NULL}, {NULL, "inc", NULL}, {NULL, "isb", NULL}, {NULL, "inx", NULL}, {NULL, "sbc", NULL}, {OP_nop, "nop47", addr_implied}, {NULL, "sbc", NULL}, {NULL, "cpx", NULL}, {NULL, "sbc", NULL}, {NULL, "inc", NULL}, {NULL, "isb", NULL}, 
@@ -76,11 +76,12 @@ int main(int argc, char* argv[]){
   c.RAM[0xFFFC] = 0x80;
   c.RAM[0xFFFD] = 0x00;
 
-  c.RAM[0x8000] = 0x4c;
+  c.RAM[0x8000] = 0x92;
   c.RAM[0x8001] = 0x02;
-  c.RAM[0x8002] = 0x00;
   
   c.registers->A = 0x41;
+  print_registers(&c);
+  
   printf("-------------- program start --------------\n");
   reset(&c);
   for(int i = 0; i < 3; i++){
@@ -89,7 +90,8 @@ int main(int argc, char* argv[]){
 
   printf("-------------- program complete --------------\n");
   printf("0x%04x : 0x%02x\n", 0x002, c.RAM[0x002]);
-  printf("0x%04x : 0x%02x\n", 0x00e, c.RAM[0x00e]);
-  //print_stack_addr(&c, STACK_TOP);
+  printf("0x%04x : 0x%02x\n", 0x005, c.RAM[0x005]);
+  printf("0x%04x : 0x%02x\n", 0x8005, c.RAM[0x8005]);
+  //Print_stack_addr(&c, STACK_TOP);
   print_registers(&c);
 }
