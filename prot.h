6491e95c969cd41d9ec2b6e26b07f3ff9d4611c4
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-30 17:02:56 +0800

    added extra mask definitions

--- prot.h
+++ prot.h
@@ -10,6 +10,15 @@
 #define FLAGS_V_MASK 0x40
 #define FLAGS_N_MASK 0x80
 
+#define BIT_0_MASK 0x1
+#define BIT_1_MASK 0x2
+#define BIT_2_MASK 0x4
+#define BIT_3_MASK 0x8
+#define BIT_4_MASK 0x10
+#define BIT_5_MASK 0x20
+#define BIT_6_MASK 0x40
+#define BIT_7_MASK 0x80
+
 
 #define STACK_BOTTOM 0x0100
 #define STACK_TOP 0x01ff
